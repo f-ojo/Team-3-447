219c763fec0f9c484efeea47ad6505eb26f7b292
NikunjPatel50 <47341824+NikunjPatel50@users.noreply.github.com>
2019-11-11 15:28:04 -0500

    REST_API Code

---
+++ AWS.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'umbc-team-3-mysql.c3kav9ktnn4z.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'ZxdkCBNxZRy82aXOPrtD'
+app.config['MYSQL_DB'] = 'team3_crime_database'
+mysql = MySQL(app)
+
+@app.route('/', methods = ['GET'])
+def cats():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('''select *
+from crime LIMIT 1000''')
+        rv = cur.fetchall()
+        payload = []
+        content = {}
+        for result in rv:
+            content = {'Date': result[0], 'CrimeTime': result[1], 'CrimeCode': result[2], 'Location' : result[3],
+                       'Description':result[4],'Inside/Outside':result[5],'Weapon':result[6], 'Post':result[7], 'District':result[8],
+                       'Neighbourhood':result[9],'Longitude':result[10], 'Latitude':result[11], 'Location 1':result[12], 'Premise':result[13],
+                       'vri_name 1':result[14], 'Total Incidents':result[15]}
+            payload.append(content)
+            content = {}
+        return jsonify(payload)
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+@app.route('/knife')
+def knife():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('''select * from crime where Weapon = "KNIFE"
+LIMIT 1000;''')
+        rv = cur.fetchall()
+        payload = []
+        content = {}
+        for result in rv:
+            content = {'Date': result[0], 'CrimeTime': result[1], 'CrimeCode': result[2], 'Location': result[3],
+                       'Description': result[4], 'Inside/Outside': result[5], 'Weapon': result[6], 'Post': result[7],
+                       'District': result[8], 'Neighbourhood': result[9], 'Longitude': result[10],
+                       'Latitude': result[11], 'Location 1': result[12], 'Premise': result[13],
+                       'vri_name 1': result[14], 'Total Incidents': result[15]}
+            payload.append(content)
+        return jsonify(payload)
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+
+def not_found(error = None):
+    message = {
+        'status' : 404,
+        'message' : 'Not Found' + request.url,
+    }
+    return jsonify(message)
+
+if __name__ == '__main__':
+   app.run(debug = True)
